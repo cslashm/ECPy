b86a0809f990af8f6529114920e22568f0248631
GoodiesHQ <GoodiesHQ@users.noreply.github.com>
2020-01-09 15:20:50 -0800

    Update curves.py

--- src/ecpy/curves.py
+++ src/ecpy/curves.py
@@ -23,7 +23,6 @@
 
 #python 2 compatibility
 from builtins import int,pow
-from math import ceil
 
 import binascii
 import random
@@ -357,7 +356,7 @@ class WeierstrassCurve(Curve):
         Returns
            bytes : encoded point [04 | x | y] or [02 | x | sign] 
         """
-        size = ceil(self.size / 8)  # Prevent overflow error
+        size = self.size >> 3 + bool(self.size % 8)
         x = bytearray(P.x.to_bytes(size,'big'))
         y = bytearray(P.y.to_bytes(size,'big'))
         if compressed:
@@ -378,7 +377,7 @@ class WeierstrassCurve(Curve):
         Returns
            Point : decoded point
         """
-        size = ceil(self.size / 8)  # Prevent overflow error
+        size = self.size>>3 + bool(self.size % 8)  # Prevent overflow error
         xy    =  bytearray(eP)
         if xy[0] == 2:
             x = xy[1:1+size]
@@ -714,7 +713,7 @@ class MontgomeryCurve(Curve):
         Returns
            bytes : encoded point
         """
-        size = ceil(self.size / 8)
+        size = self.size>>3 + bool(self.size % 8)
         x = bytearray(P.x.to_bytes(size,'little'))
         return bytes(x)
 
